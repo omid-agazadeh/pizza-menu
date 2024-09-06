4ffcf1c847c2d367a047abd5d88c8d57c2fe8d1d
omid-agazadeh <omidagazadeh.dev@gmail.com>
2024-09-06 15:19:12 +0330

    pizza-menu created

---
+++ src/components/Pizza.jsx
@@ -0,0 +1,11 @@
+function Pizza() {
+   return (
+      <div>
+        <img src="./images/pizzas/spinaci.jpg" alt="Pizza Spinaci" />
+         <h2>Pizza Spinaci</h2>
+         <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+      </div>
+   );
+}
+
+export default Pizza;
